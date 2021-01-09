e69898c9e58ca71e66cd77f1b881f0e612aa296e
Andy Su <f2280c@gmail.com>
2021-01-09 07:16:15 -0500

    Time: 0 ms (100.00%), Space: 39.4 MB (5.36%) - LeetHub

--- search-in-rotated-sorted-array/search-in-rotated-sorted-array.java
+++ search-in-rotated-sorted-array/search-in-rotated-sorted-array.java
@@ -1,47 +1,26 @@
 class Solution {
-    public int search(int[] nums, int target) {
-    //     int high = arr.length - 1;
-    //     int low = 0;
-    //     while(low <= high){
-    //         int mid = low + (high - low) / 2;
-    //         if(arr[mid] == target){
-    //             return mid;
-    //         }
-    //         if(arr[low] < arr[mid]){
-    //             if(arr[low] <= target && target < arr[mid]){
-    //                 high = mid - 1;
-    //             }else{
-    //                 low = mid + 1;
-    //             }
-    //         }else{
-    //             if(arr[mid] < target && target <= arr[high]){
-    //                 low = mid + 1;
-    //             }else{
-    //                 high = mid - 1;
-    //             }
-    //         }
-    //     }
-    //     return -1;
-    // }
-        int n = nums.length;
-        int low=0;
-        int high=n-1;
-        while(low<=high) {
-            
-            int mid= low + (high-low)/2;
-            
-            if(nums[mid] == target) {
+    public int search(int[] arr, int target) {
+        int high = arr.length - 1;
+        int low = 0;
+        while(low <= high){
+            int mid = low + (high - low) / 2;
+            if(arr[mid] == target){
                 return mid;
             }
-            if(nums[low] <= nums[mid]) {
-                
-                if(nums[low] <= target && target < nums[mid]) {
-                    high=mid-1;
+            if(arr[low] <= arr[mid]){
+                if(arr[low] <= target && target < arr[mid]){
+                    high = mid - 1;
+                }else{
+                    low = mid + 1;
                 }
-                else {
-                    low=mid+1;
-                }
+            }else{
+                if(arr[mid] < target && target <= arr[high]){
+                    low = mid + 1;
+                }else{
+                    high = mid - 1;
+                }
             }
-            else {
-                
-                if(nums[mid] < target && target <= nums[high]) {
+        }
+        return -1;
+    }
+}

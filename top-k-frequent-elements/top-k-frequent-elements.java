d2c4f02e5ab695b78dcb6087e17dd5cb36e17d48
Andy Su <f2280c@gmail.com>
2021-01-19 14:46:02 +0800

    Time: 7 ms (99.43%), Space: 41.9 MB (33.28%) - LeetHub

--- top-k-frequent-elements/top-k-frequent-elements.java
+++ top-k-frequent-elements/top-k-frequent-elements.java
@@ -1,5 +1,37 @@
 class Solution {
     public int[] topKFrequent(int[] nums, int k) {
+        return bucket(nums, k);   
+    }
+    public int[] bucket(int[] nums, int k){
+        if(k == nums.length) return nums;
+        Map<Integer, Integer> tally = new HashMap<>();
+        // tally the frequency
+        for(int n : nums){
+            tally.put(n, tally.getOrDefault(n, 0) + 1);
+        }
+        // adds the value to its frequency bucket
+        List<Integer>[] list = new ArrayList[nums.length + 1];
+        for(int n : tally.keySet()){
+            int count = tally.get(n);
+            if(list[count] == null) list[count] = new ArrayList<>();
+            list[count].add(n);
+        }
+        // starts gathering from the end of frequency list
+        List<Integer> result = new ArrayList<>();
+        for (int i = list.length - 1; i >= 0 && result.size() < k; i--) {
+            if(list[i] != null){
+                result.addAll(list[i]);
+            }
+        }
+        // convert list to int array
+        int[] arr = new int[result.size()];
+        for(int i=0; i<arr.length; i++){
+            arr[i] = result.get(i);
+        }
+        return arr;
+    }
+    
+    public int[] heapify(int[] nums, int k){
         if(k == nums.length) return nums;
         Map<Integer, Integer> tally = new HashMap<>();
         // tally the frequency
@@ -7,16 +39,3 @@ class Solution {
             tally.put(n, tally.getOrDefault(n, 0) + 1);
         }
         // sort the tally so the lowest frequency appears first
-        Queue<Integer> heap = new PriorityQueue<>((n1, n2) -> tally.get(n1) - tally.get(n2));
-        for(int n: tally.keySet()){
-            heap.add(n);
-            // poll the lowest so we only keep the "top N"
-            if(heap.size() > k) heap.poll();
-        }
-        int[] top = new int[k];
-        for(int i = 0; i<top.length; i++){
-            top[i] = heap.poll();
-        }
-        return top;
-    }
-}

899c63ba34023bd8bc63b5b7a2114d41fce8a41f
Andy Su <f2280c@gmail.com>
2021-03-07 21:19:13 +0800

    Time: 1 ms (71.52%), Space: 39.3 MB (20.16%) - LeetHub

---
+++ best-time-to-buy-and-sell-stock-ii/best-time-to-buy-and-sell-stock-ii.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int maxProfit(int[] prices) {
+        int i = 0;
+        int gain = 0;
+        while(i < prices.length){
+            // find local minimum
+            while(i < prices.length - 1 && prices[i+1] < prices[i]) i++;
+            int min = prices[i];
+            // find local maximum
+            while(i < prices.length - 1 && prices[i+1] > prices[i]) i++;
+            gain += prices[i] - min;
+            i++;
+        }
+        return gain;
+    }
+}

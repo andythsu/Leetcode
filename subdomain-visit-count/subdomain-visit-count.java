c2d761db2484341b2bed453a35e8ad822656ce2e
Andy Su <f2280c@gmail.com>
2021-03-29 22:39:46 +0800

    Time: 37 ms (11.66%), Space: 41.2 MB (26.72%) - LeetHub

---
+++ subdomain-visit-count/subdomain-visit-count.java
@@ -0,0 +1,22 @@
+class Solution {
+    public List<String> subdomainVisits(String[] cpdomains) {
+        Map<String, Integer> map = new HashMap<>();
+        
+        for(String s : cpdomains){
+            String[] split = s.split("\\s+");
+            int count = Integer.valueOf(split[0]);
+            String[] domains = split[1].split("\\.");
+            String curDomain = "";
+            for(int i = domains.length - 1; i>=0; i--){
+                curDomain = domains[i] + (i < domains.length - 1 ? "." : "") + curDomain;
+                map.put(curDomain, map.getOrDefault(curDomain, 0) + count);
+            }
+        }
+        System.out.println(map);
+        List<String> res = new ArrayList<>();
+        for(String domain : map.keySet()){
+            res.add(map.get(domain) + " " + domain);
+        }
+        return res;
+    }
+}

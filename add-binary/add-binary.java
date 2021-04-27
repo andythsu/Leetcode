0f4604b91c0caa9e74ad171469d210d390be620d
Andy Su <f2280c@gmail.com>
2021-04-27 22:42:03 +0800

    Time: 6 ms (15.08%), Space: 38.6 MB (50.43%) - LeetHub

---
+++ add-binary/add-binary.java
@@ -0,0 +1,40 @@
+class Solution {
+    public String addBinary(String a, String b) {
+        // make a and b same length
+        int aLength = a.length();
+        int bLength = b.length();
+        if(a.length() > b.length()){
+            for(int i=0; i<aLength - bLength; i++){
+                b = "0" + b;
+            }
+        }else{
+            for(int i=0; i < bLength - aLength; i++){
+                a = "0" + a;
+            }
+        }
+        System.out.println(a);
+        System.out.println(b);
+        String result = "";
+        int carry = 0;
+        for(int i=a.length()-1; i>=0; i--){
+            int aNum = Character.getNumericValue(a.charAt(i));
+            int bNum = Character.getNumericValue(b.charAt(i));
+            int sum = aNum + bNum + carry;
+            carry = 0;
+            if(sum == 0){
+                result = "0" + result;
+            }else if(sum == 1){
+                result = "1" + result;
+            }else if (sum == 2){
+                result = "0" + result;
+                carry = 1;
+            }else{
+                result = "1" + result;
+                carry = 1;
+            }
+        }
+        if(carry == 1) result = "1" + result;
+        
+        return result;
+    }
+}

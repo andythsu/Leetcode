cdd79db86eae39163bf5406ceb444e2e8f88a37a
Andy Su <f2280c@gmail.com>
2021-06-10 22:12:10 +0800

    Time: 2 ms (51.46%), Space: 36.4 MB (52.26%) - LeetHub

---
+++ permutation-sequence/permutation-sequence.java
@@ -0,0 +1,28 @@
+/**
+https://www.youtube.com/watch?v=W9SIlE2jhBQ
+*/
+class Solution {
+    public String getPermutation(int n, int k) {
+        // get permutations
+        List<Integer> avail = new ArrayList();
+        int[] fact = new int[10];
+        fact[0] = 1;
+        for(int i=1; i<=9; i++) fact[i] = fact[i-1] * i; 
+        
+        // put all avail numbers in list
+        for(int i=1; i<=n; i++) avail.add(i);
+        
+        String ans = "";
+        while(true){
+            if(avail.size() == 1){
+                return ans + avail.get(0);
+            }
+            int index = k / fact[n-1];
+            if(k % fact[n-1] == 0) index = index - 1; // because our groupings are 0-based
+            ans = ans + avail.get(index) + "";
+            avail.remove(index);
+            k = k - index * fact[n-1];
+            n = n -1;
+        }
+    }
+}

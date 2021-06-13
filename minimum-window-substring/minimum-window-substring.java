66c251b63095eb7c89588928f821f695d9231d44
Andy Su <f2280c@gmail.com>
2021-06-13 11:55:49 +0800

    Time: 8 ms (74.95%), Space: 47.6 MB (5.45%) - LeetHub

--- minimum-window-substring/minimum-window-substring.java
+++ minimum-window-substring/minimum-window-substring.java
@@ -1,28 +1,29 @@
 class Solution {
     public String minWindow(String s, String t) {
+        if(t.length() > s.length() || (s == "" && t == "")) return "";
         int[] charCount = new int[128];
         
         for(char c : t.toCharArray()) charCount[c]++;
         
-        
         int count = 0;
         int left = 0;
         int minLen = Integer.MAX_VALUE;
-        String result = "";
+        String res = "";
         
         for(int right=0; right<s.length(); right++){
-            charCount[s.charAt(right)]--;
-            if(charCount[s.charAt(right)] >= 0) count++;
+            char cur = s.charAt(right);
+            charCount[cur]--;
+            if(charCount[cur] >= 0) count++;
             while(count == t.length()){
                 if(minLen > right - left + 1){
                     minLen = right - left + 1;
-                    result = s.substring(left, left + minLen);
+                    res = s.substring(left, right+1);
                 }
                 charCount[s.charAt(left)]++;
                 if(charCount[s.charAt(left)] > 0) count--;
                 left++;
             }
         }
-        return result;
+        return res;
     }
 }

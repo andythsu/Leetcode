f07fec4ff20c709db8dc180572ecdea80a7cb04b
Andy Su <f2280c@gmail.com>
2021-06-14 18:00:38 +0800

    Time: 0 ms (100.00%), Space: 38.8 MB (76.50%) - LeetHub

--- rotate-image/rotate-image.java
+++ rotate-image/rotate-image.java
@@ -1,39 +1,22 @@
-/*
-transpose 
-1 2 3
-4 5 6
-7 8 9
-​
-1 4 7
-2 5 8
-3 6 9
-swap head and tail
-7 4 1
-8 5 2
-9 6 3
-*/
 class Solution {
-    public void rotate(int[][] matrix) {
-        matrix = swap(transpose(matrix));
-    }
-    public int[][] transpose(int[][] matrix){
-        for(int r=0; r<matrix.length; r++){
-            for(int c=r; c<matrix[r].length; c++){
-                int temp = matrix[r][c];
-                matrix[r][c] = matrix[c][r];
-                matrix[c][r] = temp;
-            }
-        }
-        return matrix;
-    }
-    public int[][] swap(int[][] matrix){
-        for(int r=0; r<matrix.length; r++){
-            for(int c=0; c<matrix[r].length / 2; c++){
-                int temp = matrix[r][c];
-                matrix[r][c] = matrix[r][matrix[r].length - 1 - c];
-                matrix[r][matrix[r].length - 1 - c] = temp;
-            }
-        }
-        return matrix;
-    }
+    public void rotate(int[][] matrix) {
+        // rotate diagonally
+        for(int i=0; i<matrix.length; i++){
+            for(int j=0; j<matrix[i].length; j++){
+                if(i < j){
+                    int temp = matrix[i][j];
+                    matrix[i][j] = matrix[j][i];
+                    matrix[j][i] = temp;
+                }
+            }
+        }
+        // switch left to right
+        for(int i=0; i<matrix.length; i++){
+            for(int j=0; j<matrix[i].length/2; j++){
+                int temp = matrix[i][j];
+                matrix[i][j] = matrix[i][matrix[i].length - 1 - j];
+                matrix[i][matrix[i].length - 1 - j] = temp;
+            }
+        }
+    }
 }

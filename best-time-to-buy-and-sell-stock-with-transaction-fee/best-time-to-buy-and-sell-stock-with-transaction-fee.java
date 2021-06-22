def3626cfc0574fdd82879e101cee61898a92ae0
Andy Su <f2280c@gmail.com>
2021-06-21 21:28:07 -0400

    Update best-time-to-buy-and-sell-stock-with-transaction-fee.java

--- best-time-to-buy-and-sell-stock-with-transaction-fee/best-time-to-buy-and-sell-stock-with-transaction-fee.java
+++ best-time-to-buy-and-sell-stock-with-transaction-fee/best-time-to-buy-and-sell-stock-with-transaction-fee.java
@@ -1,11 +1,11 @@
 class Solution {
     public int maxProfit(int[] prices, int fee) {
-        int cash = 0;
-        int hold = -prices[0];
+        int profit = 0;
+        int cost = -prices[0];
         for(int i=1; i<prices.length; i++){
-            cash = Math.max(cash, hold + prices[i] - fee);
-            hold = Math.max(hold, cash - prices[i]);
+            profit = Math.max(profit, cost + prices[i] - fee); // do nothing vs sell one today
+            cost = Math.max(cost, profit - prices[i]); // do nothing vs buy one today
         }
-        return cash;
+        return profit;
     }
 }

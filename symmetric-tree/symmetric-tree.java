3ec7fb400cae5f388da5a3aa848939cac4b1282e
Andy Su <f2280c@gmail.com>
2021-07-12 15:38:41 -0400

    Time: 1 ms (26.56%), Space: 39.3 MB (5.30%) - LeetHub

--- symmetric-tree/symmetric-tree.java
+++ symmetric-tree/symmetric-tree.java
@@ -1,35 +1,43 @@
 /**
  * Definition for a binary tree node.
  * public class TreeNode {
- *     int val;
- *     TreeNode left;
- *     TreeNode right;
- *     TreeNode() {}
- *     TreeNode(int val) { this.val = val; }
- *     TreeNode(int val, TreeNode left, TreeNode right) {
- *         this.val = val;
- *         this.left = left;
- *         this.right = right;
- *     }
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
  * }
  */
 class Solution {
-    public boolean isSymmetric(TreeNode root) {
-        Queue<TreeNode> queue = new LinkedList<>();
-        if(root == null) return true;
-        queue.add(root);
-        queue.add(root);
-        while(!queue.isEmpty()){
-            TreeNode t1 = queue.poll();
-            TreeNode t2 = queue.poll();
-            if(t1 == null && t2 == null) continue;
-            if(t1 == null || t2 == null) return false;
-            if(t1.val != t2.val) return false;
-            queue.add(t1.left);
-            queue.add(t2.right);
-            queue.add(t1.right);
-            queue.add(t2.left);
-        }
-        return true;
-    }
+    public boolean isSymmetric(TreeNode root) {
+        // return recursive(root);
+        return iterative(root);
+    }
+    public boolean iterative(TreeNode root){
+        Queue<TreeNode> queue = new LinkedList();
+        queue.add(root);
+        queue.add(root);
+        while(!queue.isEmpty()){
+            TreeNode n1 = queue.poll();
+            TreeNode n2 = queue.poll();
+            if(n1 == null && n2 == null) continue;
+            if(n1 == null || n2 == null) return false;
+            if(n1.val != n2.val) return false;
+            queue.add(n1.left);
+            queue.add(n2.right);
+            queue.add(n1.right);
+            queue.add(n2.left);
+        }
+        return true;
+    }
+    
+    public boolean recursive(TreeNode root){
+        if(root == null) return true;
+        return isSymmetric(root.left, root.right);        
+    }
+    public boolean isSymmetric(TreeNode left, TreeNode right){
+        if(left == null && right == null) return true;
+        if(left == null || right == null) return false;
+        if(left.val != right.val) return false;
+        return isSymmetric(left.left, right.right) && isSymmetric(left.right, right.left);
+    }
 }
